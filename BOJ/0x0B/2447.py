ea63d81b60cdcf9e660122e6122440586b753cc7
NanoPyro <pepper.nifi@gmail.com>
2024-02-17 21:37:12 +0900

    Updated 2447

--- BOJ/0x0B/2447.py
+++ BOJ/0x0B/2447.py
@@ -1,29 +1,21 @@
 import sys
 input = sys.stdin.readline
-write = sys.stdout.write
 
-n = int(input())
-board = [['*']*n for _ in range(n)]
-
-def blank(x:int, y: int, length:int):
-    for i in range(x, x+length):
-            for j in range(y, y+length):
-                board[i][j] = ' '
-
-def star(x: int, y: int, n: int):
-    if n == 3:
-        board[x+1][y+1] = ' '
+def star(n: int, x: int, y:int):
+    if n == 1:
+        board[x][y] = '*'
         return
-    else:
-        for i in range(3):
-            for j in range(3):
-                if i == j == 1:
-                    blank(x+i*n//3, y+j*n//3, n//3)
-                else:
-                    star(x+i*n//3, y+j*n//3, n//3)
+    for i in range(3):
+        for j in range(3):
+            if i == j == 1:
+                continue
+            star(n//3, x + n//3 * i, y + n//3 * j)
+            
+n = int(input())
+board = [[' ']*n for _ in range(n)]
+star(n, 0, 0)
 
-star(0, 0, n)
 for i in range(n):
     for j in range(n):
-        write(board[i][j])
-    write('\n')
+        sys.stdout.write(board[i][j])
+    sys.stdout.write('\n')

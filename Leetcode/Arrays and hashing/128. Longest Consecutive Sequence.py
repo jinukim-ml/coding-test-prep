7a2ceafc8470ff808205fd0022878c7ddff9e18f
NanoPyro <pepper.nifi@gmail.com>
2024-04-24 00:54:42 +0900

    Difficulty: Medium

---
+++ Leetcode/Arrays and hashing/128. Longest Consecutive Sequence.py
@@ -0,0 +1,16 @@
+def solution(nums):
+    if len(nums) == 0:
+        return 0
+    
+    nums = sorted(set(nums))
+    streak = 1
+    longest_streak = 0
+    for i in range(1, len(nums)):
+        print(f'nums[i]: {nums[i]}, nums[i-1]: {nums[i-1]}')
+        if nums[i] - nums[i-1] == 1:
+            streak += 1
+        else:
+            longest_streak = streak if longest_streak <= streak else longest_streak
+            streak = 1
+    longest_streak = streak if longest_streak <= streak else longest_streak
+    return longest_streak

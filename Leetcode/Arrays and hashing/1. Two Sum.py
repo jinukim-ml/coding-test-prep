9a467bbd4c5ef6d2c4be8907642d8b40223f38a8
NanoPyro <pepper.nifi@gmail.com>
2024-04-24 15:58:10 +0900

    Difficulty: Easy

---
+++ Leetcode/Arrays and hashing/1. Two Sum.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        table = {}
+        for i in range(len(nums)):
+            table[nums[i]] = table.get(nums[i], []) + [i]
+        nums = sorted(nums)
+        left = 0
+        right = len(nums)-1
+
+        for k in table.keys():
+            if target - k in table:
+                if k == target - k and len(table[target - k]) > 1:
+                    return[table[target - k][0], table[target - k][1]]
+                elif k != target - k:
+                    return[table[k][0], table[target-k][0]]

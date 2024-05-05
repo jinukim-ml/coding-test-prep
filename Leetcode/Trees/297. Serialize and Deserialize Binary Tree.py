3351e9aee846ebdc92202b00f1d70913da5e0d00
NanoPyro <pepper.nifi@gmail.com>
2024-05-05 21:31:20 +0900

    Difficulty: Hard

---
+++ Leetcode/Trees/297. Serialize and Deserialize Binary Tree.py
@@ -0,0 +1,54 @@
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Codec:
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root:
+            return ""
+
+        res = []
+        # preorder traversal
+
+        def dfs(node):
+            if not node:
+                res.append("N")
+                return
+
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+        
+        dfs(root)
+        return ','.join(res)
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        vals = data.split(',')
+        self.i = 0
+
+        def dfs():
+            if vals[self.i] == "N":
+                self.i += 1
+                return None
+            
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+            node.left = dfs()
+            node.right = dfs()
+            return node
+        
+        return dfs()

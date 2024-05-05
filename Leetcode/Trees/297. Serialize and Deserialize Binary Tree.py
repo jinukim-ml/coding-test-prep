bc3c7e2730a6f3bb73b8ca075dd4f761be0a5fd9
NanoPyro <pepper.nifi@gmail.com>
2024-05-05 21:57:09 +0900

    Difficulty: Hard

--- Leetcode/Trees/297. Serialize and Deserialize Binary Tree.py
+++ Leetcode/Trees/297. Serialize and Deserialize Binary Tree.py
@@ -6,7 +6,7 @@ class TreeNode:
         self.left = left
         self.right = right
 
-class Codec:
+class Codec: # Preorder traversal (DFS)
     def serialize(self, root):
         """Encodes a tree to a single string.
         
@@ -49,3 +49,46 @@ class Codec:
             return node
         
         return dfs()
+        
+class Codec2: # Level order traversal (BFS)
+    def serialize(self, root: TreeNode):
+        res = []
+
+        q = deque()
+        q.append(root)
+        
+        while q:
+            for i in range(len(q)):
+                node:TreeNode = q.popleft()
+                if node:
+                    res.append(str(node.val))
+                    q.append(node.left)
+                    q.append(node.right)
+                else:
+                    res.append('N')
+        return ','.join(res)
+    
+    def deserialize(self, data: str):
+        if data == 'N':
+            return
+        
+        data = data.split(',')
+        root = TreeNode(data[0])
+
+        q = deque()
+        q.append(root)
+
+        i = 1
+        while q and i < len(data):
+            node = q.popleft()
+
+            if data[i] != 'N':
+                node.left = TreeNode(int(data[i]))
+                q.append(node.left)
+            i += 1
+
+            if i < len(data) and data[i] != 'N':
+                node.right = TreeNode(int(data[i]))
+                q.append(node.right)
+            i += 1
+        return root

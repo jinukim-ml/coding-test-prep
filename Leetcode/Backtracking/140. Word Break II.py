2011f0cf7156df13e2ed11938eff4cc4f5f858aa
NanoPyro <pepper.nifi@gmail.com>
2024-05-25 18:42:07 +0900

    Difficulty: Hard

--- Leetcode/Backtracking/140. Word Break II.py
+++ Leetcode/Backtracking/140. Word Break II.py
@@ -2,7 +2,7 @@ from typing import List
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        # 1. can we make a word strating from i?
+        # 1. can we make a word starting from i?
         # 2. if so, when does the word end? Let's say it ends on index j.
         # 3. search again starting from index j+1
         ans = []

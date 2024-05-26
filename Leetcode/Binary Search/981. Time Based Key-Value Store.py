7199f57ba562d84909fd14296bd5b2884b02d1e2
NanoPyro <pepper.nifi@gmail.com>
2024-05-26 23:06:22 +0900

    Difficulty: Medium

---
+++ Leetcode/Binary Search/981. Time Based Key-Value Store.py
@@ -0,0 +1,30 @@
+from bisect import bisect_left
+
+class TimeMap:
+    def __init__(self):
+        self.table = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.table:
+            self.table[key] = [(value, timestamp)]
+        else:
+            idx = bisect_left(self.table[key], timestamp, key=lambda item: item[1])
+            self.table[key].insert(idx, (value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        # print(f'self.table: {self.table}')
+        # print(f'key: {key}, timestamp: {timestamp}')
+        if key not in self.table:
+            return ""
+        
+        idx = bisect_left(self.table[key], timestamp, key=lambda item: item[1])
+        if idx >= len(self.table[key]):
+            return self.table[key][-1][0]
+        else:
+            if self.table[key][idx][1] > timestamp:
+                if idx == 0:
+                    return ""
+                else:
+                    return self.table[key][idx-1][0]
+            else:
+                return self.table[key][idx][0]

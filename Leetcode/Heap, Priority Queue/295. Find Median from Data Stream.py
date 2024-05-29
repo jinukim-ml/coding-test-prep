f8ec7cac0b2fedcfb1f61b4bfe349c6c57a6d6e4
NanoPyro <pepper.nifi@gmail.com>
2024-05-30 01:13:23 +0900

    Difficulty: Hard

--- Leetcode/Heap, Priority Queue/295. Find Median from Data Stream.py
+++ Leetcode/Heap, Priority Queue/295. Find Median from Data Stream.py
@@ -1,34 +1,19 @@
-import heapq, math
+import heapq
 
 class MedianFinder:
     def __init__(self):
-        self.length = 0
+        # constantly split the whole array in half
+        # left: max heap, right: min heap
         self.left, self.right = [], []
 
     def addNum(self, num: int) -> None:
-        self.length += 1
-        # constantly split the whole array in half
-        # left: max heap, right: min heap
-        if not self.left: # both are empty
-            heapq.heappush(self.left, -num)
-        elif not self.right: # right half is empty
-            if num >= -self.left[0]:
-                heapq.heappush(self.right, num)
-            else:
-                heapq.heappush(self.right, -heapq.heappop(self.left))
-                heapq.heappush(self.left, -num)
-        else: # both not empty
-            if num < -self.left[0]:
-                heapq.heappush(self.left, -num)
-                while len(self.left) > math.ceil(self.length/2):
-                    heapq.heappush(self.right, -heapq.heappop(self.left))
-            else:
-                heapq.heappush(self.right, num)
-                while len(self.left) < math.ceil(self.length/2):
-                    heapq.heappush(self.left, -heapq.heappop(self.right))
+        if len(self.left) == len(self.right):
+            heapq.heappush(self.left, -heapq.heappushpop(self.right, num))
+        else:
+            heapq.heappush(self.right, -heapq.heappushpop(self.left, -num))
 
     def findMedian(self) -> float:
-        if self.length % 2 == 0:
-            return (-self.left[0] + self.right[0]) / 2
+        if len(self.left) == len(self.right):
+            return (self.right[0] -self.left[0]) / 2
         else:
             return -self.left[0]

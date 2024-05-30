483b98a90847ad868998959ae4b21178448bd09e
NanoPyro <pepper.nifi@gmail.com>
2024-05-31 00:35:43 +0900

    Difficulty: Medium

--- Leetcode/Trees/199. Binary Tree Right Side View.py
+++ Leetcode/Trees/199. Binary Tree Right Side View.py
@@ -6,6 +6,7 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         ans = []

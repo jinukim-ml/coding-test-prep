a1f1efd299b30a1e4bcc816352dbcda30970f5bf
NanoPyro <pepper.nifi@gmail.com>
2024-06-12 09:09:17 +0900

    Difficulty: Hard

---
+++ Leetcode/Dynamic Programming/115. Distinct Subsequences.py
@@ -0,0 +1,36 @@
+class Solution: # Bottom-up solution
+    def numDistinct(self, s: str, t: str) -> int:
+        dp = [[0 for j in range(len(t)+1)] for i in range(len(s)+1)]
+
+        for i in range(len(s)+1):
+            dp[i][len(t)] = 1
+        for j in range(len(t)):
+            dp[len(s)][j] = 0
+
+        for i in range(len(s)-1, -1, -1):
+            for j in range(len(t)-1, -1, -1):
+                if s[i] == t[j]:
+                    dp[i][j] = dp[i+1][j+1] + dp[i+1][j]
+                else:
+                    dp[i][j] = dp[i+1][j]
+        return dp[0][0]
+
+class Solution: # Top-down solution
+    def numDistinct(self, s: str, t: str) -> int:
+        dp = {}
+        def dfs(i, j):
+            if j == len(t):
+                return 1
+            if i == len(s):
+                return 0
+            if (i,j) in dp:
+                return dp[(i,j)]
+
+            if s[i] == t[j]:
+                dp[(i,j)] = dfs(i+1, j+1) + dfs(i+1, j)
+            else:
+                dp[(i,j)] = dfs(i+1, j)
+            
+            return dp[(i,j)]
+        
+        return dfs(0, 0)

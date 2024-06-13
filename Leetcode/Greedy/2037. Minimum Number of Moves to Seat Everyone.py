87319d1ce82c832a3e4089a357d47efe5fa75cf3
NanoPyro <pepper.nifi@gmail.com>
2024-06-13 13:19:13 +0900

    Difficulty: Easy

---
+++ Leetcode/Greedy/2037. Minimum Number of Moves to Seat Everyone.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        seats.sort()
+        students.sort()
+
+        ans = 0
+        for i in range(len(seats)):
+            ans += abs(seats[i] - students[i])
+        return ans

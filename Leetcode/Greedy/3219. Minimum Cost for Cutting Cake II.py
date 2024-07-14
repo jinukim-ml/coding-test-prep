a523ae27739edeb84bfce0893d0e9054f1be9cff
NanoPyro <pepper.nifi@gmail.com>
2024-07-14 13:55:25 +0900

    Difficulty: Hard

---
+++ Leetcode/Greedy/3219. Minimum Cost for Cutting Cake II.py
@@ -0,0 +1,34 @@
+from typing import List
+
+# Same solution as 3218. Minimum Cost for Cutting Cake I. This one has more challenging constraints but the gist is the same.
+
+class Solution:
+    def minimumCost(self, m: int, n: int, horizontalCut: List[int], verticalCut: List[int]) -> int:
+        horizontalCut.sort(reverse=True)
+        verticalCut.sort(reverse=True)
+
+        h, v = 0, 0
+        hcnt, vcnt = 1, 1
+        ans = 0
+
+        while h < m-1 and v < n-1:
+            if horizontalCut[h] >= verticalCut[v]:
+                ans += horizontalCut[h] * vcnt
+                hcnt += 1
+                h += 1
+            else:
+                ans += verticalCut[v] * hcnt
+                vcnt += 1
+                v += 1
+        
+        while h < m-1:
+            ans += horizontalCut[h] * vcnt
+            hcnt += 1
+            h += 1
+        
+        while v < n-1:
+            ans += verticalCut[v] * hcnt
+            vcnt += 1
+            v += 1
+        
+        return ans

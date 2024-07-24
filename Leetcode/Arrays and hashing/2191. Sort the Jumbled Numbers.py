ac41858a97e8953aeea83dc585591e479189e11b
NanoPyro <pepper.nifi@gmail.com>
2024-07-24 09:37:57 +0900

    Difficulty: Medium

--- Leetcode/Arrays and hashing/2191. Sort the Jumbled Numbers.py
+++ Leetcode/Arrays and hashing/2191. Sort the Jumbled Numbers.py
@@ -17,3 +17,8 @@ class Solution:
             converted, og_idx = ans[i]
             ans[i] = origins[og_idx]
         return ans
+    
+class Solution: # much more efficient solution
+    def sortJumbled(self, mapping: list[int], nums: list[int]) -> list[int]:
+        rule = str.maketrans({str(source): str(target) for source, target in enumerate(mapping)})
+        return sorted(nums, key= lambda x: int(str(x).translate(rule)))

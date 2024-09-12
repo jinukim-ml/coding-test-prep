8792b797b03f7d89894f10e170968c0dc5627292
NanoPyro <pepper.nifi@gmail.com>
2024-09-13 01:23:59 +0900

    Difficulty: Easy

--- Leetcode/Stack/1544. Make The String Great.py
+++ Leetcode/Stack/1544. Make The String Great.py
@@ -1,16 +1,9 @@
 class Solution:
     def makeGood(self, s: str) -> str:
-        lower = set([chr(97+i) for i in range(26)])
-        upper = set([chr(65+i) for i in range(26)])
-        stack = []        
+        stack = []
         for ch in s:
-            if stack:
-                if ch in lower and stack[-1] in upper and stack[-1].lower() == ch:
-                    stack.pop()
-                elif ch in upper and stack[-1] in lower and stack[-1].upper() == ch:
-                    stack.pop()
-                else:
-                    stack.append(ch)
+            if stack and abs(ord(ch) - ord(stack[-1])) == 32:
+                stack.pop()
             else:
                 stack.append(ch)
         return ''.join(stack)

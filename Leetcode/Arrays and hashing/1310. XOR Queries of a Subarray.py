7eee0f51a363f9c9670a36b6fa8c84abc323c103
NanoPyro <pepper.nifi@gmail.com>
2024-09-13 14:22:28 +0900

    Difficulty: Medium

--- Leetcode/Arrays and hashing/1310. XOR Queries of a Subarray.py
+++ Leetcode/Arrays and hashing/1310. XOR Queries of a Subarray.py
@@ -2,14 +2,14 @@ class Solution:
     def xorQueries(self, arr: list[int], queries: list[list[int]]) -> list[int]:
         precalc = []
         xor = 0
-        for item in arr:
-            xor ^= item
-            precalc.append(xor)
+        for i in range(len(arr)):
+            xor ^= arr[i]
+            arr[i] = xor
         
         ans = []
         for l, r in queries:
             if l > 0:
-                ans.append(precalc[l-1] ^ precalc[r])
+                ans.append(arr[l-1] ^ arr[r])
             else:
-                ans.append(precalc[r])
+                ans.append(arr[r])
         return ans

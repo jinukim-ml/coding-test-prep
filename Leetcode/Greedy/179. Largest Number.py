b521015cc5c28b8656bc54452391a52604a63250
NanoPyro <pepper.nifi@gmail.com>
2024-09-18 20:20:34 +0900

    Difficulty: Medium

---
+++ Leetcode/Greedy/179. Largest Number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def largestNumber(self, nums: list[int]) -> str:
+        nums = list(map(str, nums))
+        nums.sort(key=lambda x: x * 10, reverse=True)
+        if nums[0] == '0':
+            return '0'
+        return ''.join(nums)

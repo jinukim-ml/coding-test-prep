defa107fa5b0768d4fad3c3f6f65ba4cbc3bd027
NanoPyro <pepper.nifi@gmail.com>
2024-09-22 15:00:31 +0900

    Difficulty: Hard

---
+++ Leetcode/Tries/440. K-th Smallest in Lexicographical Order.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findKthNumber(self, n: int, k: int) -> int:
+        curr = 1
+        k -= 1
+        
+        while k:
+            step = self._count_steps(n, curr, curr + 1)
+
+            if step <= k:
+                curr += 1
+                k -= step
+            else:
+                curr *= 10
+                k -= 1
+        return curr
+    
+    def _count_steps(self, n, prefix1, prefix2):
+        steps = 0
+        while prefix1 <= n:
+            steps += min(n+1, prefix2) - prefix1
+            prefix1 *= 10
+            prefix2 *= 10
+        return steps

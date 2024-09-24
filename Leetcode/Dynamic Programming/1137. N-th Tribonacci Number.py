a9f950500bedfab9fc033ef8a55bcb130129820c
NanoPyro <pepper.nifi@gmail.com>
2024-09-24 11:55:37 +0900

    Difficulty: Easy

---
+++ Leetcode/Dynamic Programming/1137. N-th Tribonacci Number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n < 3:
+            if n == 0:
+                return 0
+            else:
+                return 1
+        else:
+            dp = [0] * (n+1)
+            dp[1], dp[2] = 1, 1
+            for i in range(3, n+1):
+                dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
+            return dp[n]

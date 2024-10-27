fa60538bb1419f4e6756747321232295a994a1f7
NanoPyro <pepper.nifi@gmail.com>
2024-10-27 14:18:39 +0900

    Difficulty: Medium

---
+++ Leetcode/Arrays and hashing/1277. Count Square Submatrices with All Ones.py
@@ -0,0 +1,29 @@
+class Solution: # brute force solution
+    def countSquares(self, matrix: list[list[int]]) -> int:
+        m, n = len(matrix), len(matrix[0])
+        ans = 0
+        for sq_size in range(1, min(m, n)+1):
+            row_start = 0
+            local_ans = 0
+            while row_start <= m - sq_size + 1:
+                col_start = 0
+                while col_start <= n - sq_size + 1:
+                    cnt = 0
+                    for r in range(row_start, row_start + sq_size):
+                        for c in range(col_start, col_start + sq_size):
+                            if r < m and c < n and matrix[r][c] == 1:
+                                cnt += 1
+                            else:
+                                break
+                        else:
+                            continue
+                        break
+                    
+                    if cnt == sq_size ** 2:
+                        local_ans += 1
+                    col_start += 1
+                
+                row_start += 1
+            ans += local_ans
+            
+        return ans

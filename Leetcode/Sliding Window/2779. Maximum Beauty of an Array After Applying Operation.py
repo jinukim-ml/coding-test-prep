540fb0fc431412df6d66b612f730b9d50a9bb88b
NanoPyro <pepper.nifi@gmail.com>
2024-12-11 22:41:43 +0900

    Difficulty: Medium

---
+++ Leetcode/Sliding Window/2779. Maximum Beauty of an Array After Applying Operation.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximumBeauty(self, nums: list[int], k: int) -> int:
+        nums.sort()
+        l = 0
+        res = 0
+        for r in range(len(nums)):
+            while nums[r] - nums[l] > 2*k:
+                l += 1
+            res = max(res, r - l + 1)
+        return res

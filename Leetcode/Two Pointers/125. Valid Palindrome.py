b9158839fe456464a33e7585e7597132d12aa340
NanoPyro <pepper.nifi@gmail.com>
2025-03-02 22:45:58 +0900

    Difficulty: Easy

--- Leetcode/Two Pointers/125. Valid Palindrome.py
+++ Leetcode/Two Pointers/125. Valid Palindrome.py
@@ -1,5 +1,5 @@
 import re
-class Solution:
+class Solution: # Solution using regex
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
         s = ''.join(re.findall('[a-z0-9]', s))
@@ -7,4 +7,18 @@ class Solution:
         for i in range(len(s)):
             if s[i] != s[len(s)-(i+1)]:
                 return False
+        return True
+
+class Solution: # Two pointers solution
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s)-1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
         return True

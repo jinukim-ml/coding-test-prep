126ef95063c5f11d9ce02b4a485e2c74785dbf43
NanoPyro <pepper.nifi@gmail.com>
2025-03-30 15:32:21 +0900

    Difficulty: Medium

--- Leetcode/Greedy/763. Partition Labels.py
+++ Leetcode/Greedy/763. Partition Labels.py
@@ -1,18 +1,34 @@
-from typing import List
-
 class Solution:
-    def partitionLabels(self, s: str) -> List[int]:
+    def partitionLabels(self, s: str) -> list[int]:
         last = {}
         for i, ch in enumerate(s):
             last[ch] = i
         start, end, curr = 0, 0, 0
         ans = []
-        while curr < len(s):
+        for curr in range(len(s)):
             end = max(end, last[s[curr]])
             if curr == end:
                 ans.append(end - start + 1)
                 start = end + 1
-                curr = start
-            else:
-                curr += 1
         return ans
+
+from collections import Counter, defaultdict
+
+class Solution:
+    def partitionLabels(self, s: str) -> list[int]:
+        counter = Counter(s)
+        curr = defaultdict(int)
+        res = []
+        l, r = 0, 0
+        while r < len(s):
+            curr[s[r]] += 1
+            counter[s[r]] -= 1
+            for ch in curr.keys():
+                if counter[ch]:
+                    break
+            else:
+                curr = defaultdict(int)
+                res.append(r - l + 1)
+                l = r + 1
+            r += 1
+        return res

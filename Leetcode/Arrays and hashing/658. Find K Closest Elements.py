7ddc501d6d4156a0a1b66f09adba403ebc155bd1
NanoPyro <pepper.nifi@gmail.com>
2025-04-04 06:40:31 +0900

    Difficulty: Medium

--- Leetcode/Arrays and hashing/658. Find K Closest Elements.py
+++ Leetcode/Arrays and hashing/658. Find K Closest Elements.py
@@ -11,3 +11,13 @@ class Solution: # O(n log n) solution using min heap
             _, i = heapq.heappop(h)
             res.append(arr[i])
         return sorted(res)
+
+class Solution: # O(n) two pointer solution
+    def findClosestElements(self, arr: list[int], k: int, x: int) -> list[int]:
+        l, r = 0, len(arr)-1
+        while r - l + 1 > k:
+            if abs(arr[l] - x) > abs(arr[r] - x):
+                l += 1
+            else:
+                r -= 1
+        return arr[l:r+1]

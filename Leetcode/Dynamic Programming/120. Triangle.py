f66f33225923027a4e8678c981971394e9a1b9f5
NanoPyro <pepper.nifi@gmail.com>
2025-04-07 13:13:23 +0900

    Difficulty: Medium

---
+++ Leetcode/Dynamic Programming/120. Triangle.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minimumTotal(self, triangle: list[list[int]]) -> int:
+        if len(triangle) == 1:
+            return triangle[0][0]
+        prev = triangle[-1]
+        for i in range(len(triangle)-2, -1, -1):
+            for j in range(len(triangle[i])):
+                triangle[i][j] += min(prev[j], prev[j+1])
+            prev = triangle[i]
+        return triangle[0][0]

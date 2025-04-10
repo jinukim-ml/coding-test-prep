9d9505500216f8682ec243c85e9edca8e0103d6b
NanoPyro <pepper.nifi@gmail.com>
2025-04-10 13:49:09 +0900

    Difficulty: Easy

--- Leetcode/Arrays and hashing/2185. Counting Words With a Given Prefix.py
+++ Leetcode/Arrays and hashing/2185. Counting Words With a Given Prefix.py
@@ -8,3 +8,40 @@ class Solution:
             if i == len(pref):
                 res += 1
         return res
+
+# Trie solution
+from collections import defaultdict
+
+class TrieNode:
+    def __init__(self) -> None:
+        self.children = defaultdict(TrieNode)
+        self.cnt = 0
+
+class Trie:
+    def __init__(self) -> None:
+        self.root = TrieNode()
+    
+    def insert(self, word: str) -> None:
+        node = self.root
+        for ch in word:
+            if ch in node.children:
+                node = node.children[ch]
+                node.cnt += 1
+            else:
+                newnode = TrieNode()
+                node.children[ch] = newnode
+                node = node.children[ch]
+                node.cnt += 1
+    
+    def count(self, word: str) -> int:
+        node = self.root
+        for ch in word:
+            node = node.children[ch]
+        return node.cnt
+    
+class Solution:
+    def prefixCount(self, words: list[str], pref: str) -> int:
+        trie = Trie()
+        for w in words:
+            trie.insert(w)
+        return trie.count(pref)

f0e4d85792b6e384d99948f880e9ba2413a3cdef
NanoPyro <pepper.nifi@gmail.com>
2025-04-27 13:49:48 +0900

    Difficulty: Easy

--- Leetcode/Sliding Window/3392. Count Subarrays of Length Three With a Condition.py
+++ Leetcode/Sliding Window/3392. Count Subarrays of Length Three With a Condition.py
@@ -1,10 +1,7 @@
 class Solution:
     def countSubarrays(self, nums: list[int]) -> int:
-        l = 0
         res = 0
         for r in range(2, len(nums)):
-            if r - l + 1 > 3:
-                l += 1
-            if nums[r] + nums[l] == nums[r-1]/2:
+            if (nums[r] + nums[r-2])*2 == nums[r-1]:
                 res += 1
         return res

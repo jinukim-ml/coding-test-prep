f774264a88b7b0e0a0984359e9a38cc39bb9941b
NanoPyro <pepper.nifi@gmail.com>
2025-05-02 16:42:31 +0900

    Difficulty: Easy

---
+++ Leetcode/Arrays and hashing/1051. Height Checker.py
@@ -0,0 +1,8 @@
+class Solution:
+    def heightChecker(self, heights: list[int]) -> int:
+        res = 0
+        expected = sorted(heights)
+        for i in range(len(heights)):
+            if heights[i] != expected[i]:
+                res += 1
+        return res

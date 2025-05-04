b6007ecb4a08b6a49d70ec3357c37a76d52186cb
NanoPyro <pepper.nifi@gmail.com>
2025-05-04 16:10:06 +0900

    Difficulty: Easy

--- Leetcode/Arrays and hashing/1128. Number of Equivalent Domino Pairs.py
+++ Leetcode/Arrays and hashing/1128. Number of Equivalent Domino Pairs.py
@@ -13,3 +13,16 @@ class Solution:
             if v > 1:
                 res += ((v-1)*v)//2
         return res
+
+class Solution: # more streamlined solution
+    def numEquivDominoPairs(self, dominoes: list[list[int]]) -> int:
+        freq = defaultdict(int)
+        res = 0
+        for a, b in dominoes:
+            if a >= b:
+                res += freq[(a,b)]
+                freq[(a,b)] += 1
+            else:
+                res += freq[(b,a)]
+                freq[(b,a)] += 1
+        return res

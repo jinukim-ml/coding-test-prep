3feab134887315638dd266388cc26f84e3ea114b
NanoPyro <pepper.nifi@gmail.com>
2025-05-07 01:47:56 +0900

    Difficulty: Hard

--- Leetcode/Sliding Window/239. Sliding Window Maximum.py
+++ Leetcode/Sliding Window/239. Sliding Window Maximum.py
@@ -1,5 +1,5 @@
 import heapq
-class Solution:
+class Solution: # O(n log n)
     def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
         h = []
         res = []
@@ -9,4 +9,21 @@ class Solution:
                 while h and h[0][1] < i-k+1:
                     heapq.heappop(h)
                 res.append(-h[0][0])
+        return res
+
+from collections import deque
+class Solution: # O(n)
+    def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
+        dq = deque()
+        res = []
+        l = 0
+        for r in range(len(nums)):
+            while dq and nums[dq[-1]] < nums[r]:
+                dq.pop()
+            dq.append(r)
+            if l > dq[0]:
+                dq.popleft()
+            if r >= k-1:
+                res.append(nums[dq[0]])
+                l += 1
         return res

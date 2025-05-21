da73daed100d91a91030afe78237af9b15f01413
NanoPyro <pepper.nifi@gmail.com>
2025-05-21 11:41:58 +0900

    Difficulty: Medium

--- Leetcode/Math and Geometry/73. Set Matrix Zeroes.py
+++ Leetcode/Math and Geometry/73. Set Matrix Zeroes.py
@@ -1,60 +1,47 @@
-from typing import List
-
-class Solution:
-    def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        changed = {}
-        i = 0
-        while i < len(matrix):
-            j = 0
-            
-            while j < len(matrix[0]):
-                
-                if matrix[i][j] == 0 and (i,j) not in changed:
-                    col = 0
-                    while col < len(matrix[0]): # change row
-                        if matrix[i][col] == 0:
-                            col += 1
-                            continue
-                        matrix[i][col] = 0
-                        changed[(i,col)] = True
-                        col += 1
-                    
-                    row = 0
-                    while row < len(matrix): # change column
-                        if matrix[row][j] == 0:
-                            row += 1
-                            continue
-                        matrix[row][j] = 0
-                        changed[(row,j)] = True
-                        row += 1
-                    
-                j += 1
-            i += 1
-
-class Solution: # faster solution
-    def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        m = len(matrix)
-        n = len(matrix[0])
-        
-        zero_col = set()
-        zero_row = set()
-        
+class Solution: # time complexity: O(mn), space complexity: O(m+n)
+    def setZeroes(self, matrix: list[list[int]]) -> None:
+        m, n = len(matrix), len(matrix[0])
+        h = set()
+        v = set()
         for r in range(m):
             for c in range(n):
                 if matrix[r][c] == 0:
-                    zero_col.add(c)
-                    zero_row.add(r)
-                
-        for r in zero_row:
-            for c in range(n):
+                    h.add(r)
+                    v.add(c)
+        
+        for r in h:
+            matrix[r] = [0]*n
+        for c in v:
+            for r in range(m):
                 matrix[r][c] = 0
+
+class Solution: # time complexity: O(mn), space complexity: O(1)
+    def setZeroes(self, matrix: list[list[int]]) -> None:
+        m, n = len(matrix), len(matrix[0])
+        r_flag, c_flag = False, False
+        for c in range(n):
+            if matrix[0][c] == 0:
+                r_flag = True
+                break
         
         for r in range(m):
-            for c in zero_col:
-                matrix[r][c] = 0
+            if matrix[r][0] == 0:
+                c_flag = True
+                break
+        
+        for r in range(1, m):
+            for c in range(1, n):
+                if matrix[r][c] == 0:
+                    matrix[r][0] = 0
+                    matrix[0][c] = 0
+        
+        for r in range(1, m):
+            for c in range(1, n):
+                if matrix[r][0] == 0 or matrix[0][c] == 0:
+                    matrix[r][c] = 0
+
+        if r_flag: # if true, set the first row to zeros
+            matrix[0] = [0]*n
+        if c_flag:
+            for r in range(m):
+                matrix[r][0] = 0

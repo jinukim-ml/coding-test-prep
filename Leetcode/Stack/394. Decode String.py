cf671210404587f983d2eb735661677ff13687dc
NanoPyro <pepper.nifi@gmail.com>
2025-06-11 09:49:02 +0900

    Difficulty: Medium

---
+++ Leetcode/Stack/394. Decode String.py
@@ -0,0 +1,22 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        res = ''
+        for ch in s:
+            if ch == ']':
+                reverse_stack = []
+                while stack[-1] != '[':
+                    reverse_stack.append(stack.pop())
+                string = ''
+                while reverse_stack:
+                    string += reverse_stack.pop()
+                stack.pop() # left paranthesis
+                number = 0
+                length = 0
+                while stack and stack[-1].isdigit():
+                    number += int(stack.pop()) * 10**length
+                    length += 1
+                stack.append(number * string)
+            else:
+                stack.append(ch)
+        return ''.join(stack)

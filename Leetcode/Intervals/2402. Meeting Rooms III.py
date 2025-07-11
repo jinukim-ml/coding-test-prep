91ea3b5803d060aa67b499a771f98505b726721b
NanoPyro <pepper.nifi@gmail.com>
2025-07-11 14:39:12 +0900

    Difficulty: Hard

---
+++ Leetcode/Intervals/2402. Meeting Rooms III.py
@@ -0,0 +1,29 @@
+import heapq
+
+class Solution:
+    def mostBooked(self, n: int, meetings: list[list[int]]) -> int:
+        meetings.sort()
+        counts = [0 for _ in range(n)]
+        available, in_use = [], []
+        for room in range(n):
+            heapq.heappush(available, room)
+        for i in range(len(meetings)):
+            while in_use and in_use[0][0] <= meetings[i][0]:
+                _, room = heapq.heappop(in_use)
+                heapq.heappush(available, room)
+            if available:
+                room = heapq.heappop(available)
+                heapq.heappush(in_use, (meetings[i][1], room))
+            else:
+                end_time, room = heapq.heappop(in_use)
+                diff = end_time - meetings[i][0]
+                end_time = meetings[i][1] + diff
+                heapq.heappush(in_use, (end_time, room))
+            counts[room] += 1
+        
+        res, val = 0, 0
+        for room in range(n):
+            if counts[room] > val:
+                val = counts[room]
+                res = room
+        return res

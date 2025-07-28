80adb6d8d2187871a24966060b031f9434a9df6e
NanoPyro <pepper.nifi@gmail.com>
2025-07-28 09:37:45 +0900

    Difficulty: Medium

--- Leetcode/Backtracking/2044. Count Number of Maximum Bitwise-OR Subsets.py
+++ Leetcode/Backtracking/2044. Count Number of Maximum Bitwise-OR Subsets.py
@@ -19,3 +19,19 @@ class Solution:
         
         backtrack(0, 0)
         return ans
+
+class Solution: # solution #2
+    def countMaxOrSubsets(self, nums: list[int]) -> int:
+        pair = [0, 0]
+        def backtrack(i: int, val: int):
+            if i == len(nums):
+                if val > pair[0]:
+                    pair[0] = val
+                    pair[1] = 1
+                elif val == pair[0]:
+                    pair[1] += 1
+                return
+            backtrack(i+1, val|nums[i])
+            backtrack(i+1, val)
+        backtrack(0, 0)
+        return pair[1]

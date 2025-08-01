2acb2a2b550b5a63e0e27b1a8d6c733b4b8ede75
NanoPyro <pepper.nifi@gmail.com>
2025-08-01 09:24:22 +0900

    Difficulty: Easy

--- Leetcode/Dynamic Programming/118. Pascal's Triangle.py
+++ Leetcode/Dynamic Programming/118. Pascal's Triangle.py
@@ -1,7 +1,5 @@
-from typing import List
-
-class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
+class Solution: # factorial
+    def generate(self, numRows: int) -> list[list[int]]:
         table = {}
         def factorial(target):
             if target in table:
@@ -19,3 +17,17 @@ class Solution:
             ans.append(row)
         
         return ans
+    
+class Solution: # DP
+    def generate(self, numRows: int) -> list[list[int]]:
+        res = [[1]]
+        for i in range(2, numRows+1):
+            row = []
+            for j in range(i):
+                if 0 < j < i-1:
+                    val = res[-1][j-1] + res[-1][j]
+                else:
+                    val = 1
+                row.append(val)
+            res.append(row)
+        return res

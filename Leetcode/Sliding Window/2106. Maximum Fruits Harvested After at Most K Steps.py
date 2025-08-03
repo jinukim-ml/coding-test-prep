ce3ac193ae14443472d86df17de6dc93abebca97
NanoPyro <pepper.nifi@gmail.com>
2025-08-03 12:03:02 +0900

    Difficulty: Hard

--- Leetcode/Sliding Window/2106. Maximum Fruits Harvested After at Most K Steps.py
+++ Leetcode/Sliding Window/2106. Maximum Fruits Harvested After at Most K Steps.py
@@ -4,13 +4,11 @@ class Solution:
         window = 0
         for r in range(len(fruits)):
             window += fruits[r][1]
-            cost = fruits[r][0] - fruits[l][0] + min(abs(startPos - fruits[l][0]), abs(fruits[r][0] - startPos))
-            while cost > k:
+            while fruits[r][0] - fruits[l][0] + min(abs(startPos - fruits[l][0]), abs(fruits[r][0] - startPos)) > k:
                 window -= fruits[l][1]
                 l += 1
                 if l > r:
                     break
-                cost = fruits[r][0] - fruits[l][0] + min(abs(startPos - fruits[l][0]), abs(fruits[r][0] - startPos))
             res = max(res, window)
         return res
 

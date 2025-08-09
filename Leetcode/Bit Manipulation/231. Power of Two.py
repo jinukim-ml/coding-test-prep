611a3f4d7f79a738e9603b13f6808181d0f0e0d3
Jinu Kim <jinu_kim@yonsei.ac.kr>
2025-08-09 10:12:09 +0900

    Difficulty: Easy

--- Leetcode/Bit Manipulation/231. Power of Two.py
+++ Leetcode/Bit Manipulation/231. Power of Two.py
@@ -7,3 +7,13 @@ class Solution:
             else:
                 start <<= 1
         return False
+
+class Solution: # no loop solution
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        if n == 1:
+            return True
+        if bin(n)[-1] == 1 or n.bit_count() > 1:
+            return False
+        return True

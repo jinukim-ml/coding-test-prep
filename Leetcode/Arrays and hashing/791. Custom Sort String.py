9b2f957a5ff3fd76761223d509b8d7c5f1993f3b
Jinu Kim <jinu_kim@yonsei.ac.kr>
2025-08-22 09:16:06 +0900

    Difficulty: Medium

--- Leetcode/Arrays and hashing/791. Custom Sort String.py
+++ Leetcode/Arrays and hashing/791. Custom Sort String.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution: # O(n log n)
     def customSortString(self, order: str, s: str) -> str:
         priorities = [float('inf') for _ in range(26)]
         for i, ch in enumerate(order):
@@ -8,4 +8,15 @@ class Solution:
         res = ''
         for ch, _ in s:
             res += ch
+        return res
+
+class Solution: # O(n)
+    def customSortString(self, order: str, s: str) -> str:
+        freq = Counter(s)
+        res = ''
+        for ch in order:
+            if ch in freq:
+                res += ch * freq.pop(ch)
+        for ch in freq:
+            res += ch * freq[ch]
         return res

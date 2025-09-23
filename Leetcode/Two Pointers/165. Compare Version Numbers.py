75ef9bd19d3d1b1dad6f8d17c171b623a79ce9f9
Jinu Kim <jinu_kim@yonsei.ac.kr>
2025-09-23 10:50:18 +0900

    Difficulty: Medium

--- Leetcode/Two Pointers/165. Compare Version Numbers.py
+++ Leetcode/Two Pointers/165. Compare Version Numbers.py
@@ -1,30 +1,23 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        v1 = list(map(int, version1.split('.')))
-        v2 = list(map(int, version2.split('.')))
+        version1 = version1.split('.')
+        version2 = version2.split('.')
 
-        comp = 0
-        shortest = min(len(v1), len(v2))
         i = 0
-        while i < shortest:
-            if v1[i] > v2[i]:
+        while i < min(len(version1), len(version2)):
+            a, b = int(version1[i]), int(version2[i])
+            if a > b:
                 return 1
-            elif v1[i] < v2[i]:
+            elif a < b:
                 return -1
             i += 1
         
-        if len(v1) > len(v2):
-            for j in range(i, len(v1)):
-                if v1[j] > 0:
-                    comp += 1
-        elif len(v1) < len(v2):
-            for j in range(i, len(v2)):
-                if v2[j] > 0:
-                    comp -=1
-
-        if comp > 0:
-            return 1
-        elif comp < 0:
-            return - 1
+        if len(version1) >= len(version2):
+            for j in range(i, len(version1)):
+                if int(version1[j]):
+                    return 1
         else:
-            return 0
+            for j in range(i, len(version2)):
+                if int(version2[j]):
+                    return -1
+        return 0

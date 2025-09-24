405e77957c800ca000995225e25465b766699eb9
Jinu Kim <jinu_kim@yonsei.ac.kr>
2025-09-24 13:26:39 +0900

    Difficulty: Easy

--- Leetcode/Dynamic Programming/1137. N-th Tribonacci Number.py
+++ Leetcode/Dynamic Programming/1137. N-th Tribonacci Number.py
@@ -11,3 +11,19 @@ class Solution:
             for i in range(3, n+1):
                 dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
             return dp[n]
+
+class Solution: # space complexity O(1)
+    def tribonacci(self, n: int) -> int:
+        if n < 3:
+            if n == 0:
+                return 0
+            else:
+                return 1
+        prev3, prev2, prev = 0, 1, 1
+        res = 0
+        for _ in range(3, n+1):
+            res = prev3 + prev2 + prev
+            prev3 = prev2
+            prev2 = prev
+            prev = res
+        return res

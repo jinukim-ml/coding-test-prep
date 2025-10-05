61ed47f59580be86d9342b8b2daa135e166e7daf
Jinu Kim <jinu_kim@yonsei.ac.kr>
2025-10-05 21:37:07 +0900

    Difficulty: Medium

--- Leetcode/Graphs/417. Pacific Atlantic Water Flow.py
+++ Leetcode/Graphs/417. Pacific Atlantic Water Flow.py
@@ -1,29 +1,40 @@
-from typing import List
+from collections import deque
 
 class Solution:
-    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        self.heights = heights
-        ans = []
-        for r in range(len(heights)):
-            for c in range(len(heights[0])):
-                self.vis = {(r,c)}
-                self.pac, self.atl = False, False
-                self.dfs((r,c))
-                if self.pac and self.atl:
-                    ans.append([r,c])
-
-        return ans
-    def dfs(self, coords: tuple):
-        r, c = coords
-        if r == 0 or c == 0:
-            self.pac = True
-        if r == len(self.heights) - 1 or c == len(self.heights[0]) - 1:
-            self.atl = True
-
-        for d in [(0,1), (1,0), (0,-1), (-1,0)]:
-            dr, dc = r + d[0], c + d[1]
-            if (dr,dc) not in self.vis and 0 <= dr < len(self.heights) and 0 <= dc < len(self.heights[0]) and self.heights[r][c] >= self.heights[dr][dc]:
-                self.vis.add((dr,dc))
-                self.dfs((dr,dc))
-            if self.pac and self.atl:
-                return
+    def pacificAtlantic(self, heights: list[list[int]]) -> list[list[int]]:
+        m, n = len(heights), len(heights[0])
+        directions = [(-1,0), (0,1), (1,0), (0,-1)]
+        
+        pacific, atlantic = deque(), deque()
+        for r in range(m):
+            pacific.append((r,0))
+            atlantic.append((r, n-1))
+        for c in range(1, n):
+            pacific.append((0,c))
+            atlantic.append((m-1, c))
+        atlantic.pop() # remove the duplicate (m-1,n-1)
+        atlantic.append((m-1,0))
+        
+        visited_pacific = set()
+        while pacific:
+            r, c = pacific.popleft()
+            visited_pacific.add((r,c))
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < m and 0 <= nc < n and (nr,nc) not in visited_pacific and heights[nr][nc] >= heights[r][c]:
+                    pacific.append((nr,nc))
+        
+        visited_atlantic = set()
+        while atlantic:
+            r, c = atlantic.popleft()
+            visited_atlantic.add((r,c))
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < m and 0 <= nc < n and (nr,nc) not in visited_atlantic and heights[nr][nc] >= heights[r][c]:
+                    atlantic.append((nr,nc))
+        
+        res = []
+        for r, c in visited_pacific:
+            if (r,c) in visited_atlantic:
+                res.append([r,c])
+        return res

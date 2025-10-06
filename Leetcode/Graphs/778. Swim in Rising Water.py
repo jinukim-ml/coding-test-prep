8420ca806647bd7c5b7c84589b83836c294acd2c
Jinu Kim <jinu_kim@yonsei.ac.kr>
2025-10-06 10:35:47 +0900

    Difficulty: Hard

--- Leetcode/Graphs/778. Swim in Rising Water.py
+++ Leetcode/Graphs/778. Swim in Rising Water.py
@@ -1,23 +1,20 @@
-from typing import List
 import heapq
 
 class Solution:
-    def swimInWater(self, grid: List[List[int]]) -> int:
-        directions = [(0,1), (1,0), (0,-1), (-1,0)]
-        vis = set()
-        vis.add((0,0))
+    def swimInWater(self, grid: list[list[int]]) -> int:
+        n = len(grid)
         h = [(grid[0][0], 0, 0)]
-
-        ans = grid[0][0]
+        directions = [(-1,0), (0,1), (1,0), (0,-1)]
+        visited = set()
         while h:
-            height, r, c = heapq.heappop(h)
-            ans = max(ans, height)
-            if r == len(grid)-1 and c == len(grid[0])-1:
-                return ans
-            
+            t, r, c = heapq.heappop(h)
+            if (r,c) in visited:
+                continue
+            visited.add((r,c))
+            if r == n-1 and c == n-1:
+                return t
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
-
-                if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and (nr,nc) not in vis:
-                    heapq.heappush(h, (grid[nr][nc], nr, nc))
-                    vis.add((nr,nc))
+                if 0 <= nr < n and 0 <= nc < n and (nr,nc) not in visited:
+                    new_t = max(t, grid[nr][nc])
+                    heapq.heappush(h, (new_t, nr, nc))

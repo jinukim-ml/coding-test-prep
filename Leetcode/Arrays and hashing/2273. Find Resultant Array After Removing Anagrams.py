14632ce86cc85256de3ed653d70ad810201dcf1f
Jinu Kim <jinu_kim@yonsei.ac.kr>
2025-10-13 11:05:35 +0900

    Difficulty: Easy

--- Leetcode/Arrays and hashing/2273. Find Resultant Array After Removing Anagrams.py
+++ Leetcode/Arrays and hashing/2273. Find Resultant Array After Removing Anagrams.py
@@ -15,3 +15,12 @@ class Solution: # same idea but faster
             if sorted(words[i]) != sorted(words[i-1]):
                 res.append(words[i])
         return res
+
+class Solution: # redundancy removed
+    def removeAnagrams(self, words: list[str]) -> list[str]:
+        res = [words[0]]
+        arr = [sorted(words[i]) for i in range(len(words))]
+        for i in range(1, len(words)):
+            if arr[i] != arr[i-1]:
+                res.append(words[i])
+        return res

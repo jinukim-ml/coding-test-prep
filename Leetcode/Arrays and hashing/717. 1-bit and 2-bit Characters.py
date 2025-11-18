a25013642c43f62d377ed905691839fcddb929ad
Jinu Kim <jinu_kim@yonsei.ac.kr>
2025-11-18 13:06:01 +0900

    Difficulty: Medium

---
+++ Leetcode/Arrays and hashing/717. 1-bit and 2-bit Characters.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isOneBitCharacter(self, bits: list[int]) -> bool:
+        i = 0
+        while i < len(bits)-2:
+            if bits[i] == 1:
+                i += 1
+            i += 1
+        return bits[i] == 0
